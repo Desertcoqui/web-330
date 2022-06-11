10a739b7fb1170394394bfd80ddea9f3ef5d2c52
desertcoqui <fdetres@my365.bellevue.edu>
2022-06-11 17:12:16 -0500

    main-course import fixed

--- week-3/index.js
+++ week-3/index.js
@@ -1,7 +1,18 @@
+
+// Title: Assignment 3
+// Author: Professor Kumar
+// Date: 9 June 21
+// Modified By: Ferdinand "Papo" Detres Jr
+// Description: This week's project is a Restaurant App
+// Code Attributions: The classes and id's were taken from the CSS styleguide provided by Bellevue.
+// Import and Export code came from assignment instructions
+// https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+// https://rollbar.com/blog/javascript-constructors/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Array
+// https://www.w3schools.com/jsref/prop_checkbox_value.asp
+// https://www.tutorialspoint.com/es6/es6_modules.htm
+// https://www.youtube.com/watch?v=Q3SBogjUfMk
 // https://www.spritely.net/how-to-get-checkbox-number-values-in-javascript/
-
-
-
 export * from "./appetizer.js";
 export * from "./beverage.js";
 export * from "./dessert.js";
--- week-3/main-course.js
+++ week-3/main-course.js
@@ -1,3 +1,16 @@
+// Title: Assignment 3
+// Author: Professor Kumar
+// Date: 9 June 21
+// Modified By: Ferdinand "Papo" Detres Jr
+// Description: This week's project is a Restaurant App
+// Code Attributions: The classes and id's were taken from the CSS styleguide provided by Bellevue.
+// https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+// https://rollbar.com/blog/javascript-constructors/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Array
+// https://www.w3schools.com/jsref/prop_checkbox_value.asp
+// https://www.tutorialspoint.com/es6/es6_modules.htm
+// https://www.youtube.com/watch?v=Q3SBogjUfMk
+
 import {Product} from './product.js';
 
 export class MainCourse extends Product{
