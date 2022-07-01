20f2a4fda0925b51dfcbe469abe78350ca9f506e
desertcoqui <fdetres@my365.bellevue.edu>
2022-07-01 12:20:14 -0500

    initial comments

---
+++ week-6/float-min-field.js
@@ -0,0 +1,6 @@
+// Title: Future Value App
+// Author: Professor Kumar
+// Modified by: Ferdinand Detres
+// Date: July 1 2022
+// Description: Javascript class with a constructor
+//Code came from class instructions
